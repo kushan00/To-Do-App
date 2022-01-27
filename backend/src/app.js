e9e90523b85151c1f8e0e285c97eac248c73853c
kushan00 <kushandimantha533@gmail.com>
2022-01-27 20:44:54 +0530

    ss

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import apiRoute from './utils/api.js';
+
+const app = express();
+
+
+const PORT= 8000;
+
+app.use('/api/',apiRoute);
+
+app.listen(PORT,()=> console.log('Server is up and running'))
+
